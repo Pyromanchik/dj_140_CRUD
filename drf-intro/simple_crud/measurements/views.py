92ce1d72b1b88f09f5df0c50b50f62e0bbcd1d21
Pyromanchik <romanap2902@gmail.com>
2026-06-25 12:48:38 +0400

    Initial commit

---
+++ drf-intro/simple_crud/measurements/views.py
@@ -0,0 +1,20 @@
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
+
+from .models import Product, Stock
+from .serializers import ProductSerializer, StockSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'description']
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['products']
+    search_fields = ['positions__product__title', 'positions__product__description']
